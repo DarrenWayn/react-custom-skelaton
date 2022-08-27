823d2c0f7b33e71b5f7cc14838c76471c6e41831
Darren Wayn <darrenwayn98@gmail.com>
2022-08-27 11:33:46 +0700

    first map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      shop: "Shop Now",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      shop: "Shop Now",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      shop: "Shop Now",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      shop: "Shop Now",
+    },
+
+    {
+      id: 5,
+      title: "Mens",
+      shop: "Shop Now",
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return (
+          <div className="category-container" key={category.id}>
+            {/* <img src="" /> */}
+            <div className='background-image' />
+            <div className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>{category.shop}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
